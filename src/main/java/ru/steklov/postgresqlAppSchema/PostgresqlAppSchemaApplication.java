3d78bf2948fd33d39e6193e90b2b0720dbcc6d59
Vladimir Steklov <vova.steklov@gmail.com>
2018-10-21 22:43:16 +0300

    add spring initializr files

---
+++ src/main/java/ru/steklov/postgresqlAppSchema/PostgresqlAppSchemaApplication.java
@@ -0,0 +1,12 @@
+package ru.steklov.postgresqlAppSchema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlAppSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlAppSchemaApplication.class, args);
+	}
+}
