12868b4d5f16a1e909afeb52bc1927026c3aa79f
Vladimir Steklov <vova.steklov@gmail.com>
2018-10-29 22:02:50 +0300

    add liquibase configuration and changelog

--- src/main/java/ru/steklov/postgresqlAppSchema/PostgresqlAppSchemaApplication.java
+++ src/main/java/ru/steklov/postgresqlAppSchema/PostgresqlAppSchemaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PostgresqlAppSchemaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PostgresqlAppSchemaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresqlAppSchemaApplication.class, args);
+    }
 }
